a4688436b0330e9b4696cd662c7218164997f938
ANDRES BECERRA <31633615+Andresbecerra72@users.noreply.github.com>
2021-08-19 18:10:22 -0500

    Initial Commit

--- usergrid/ContentView.swift
+++ usergrid/ContentView.swift
@@ -8,11 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let items = Array(1...10).map({"image\($0)"})
+    
+    let layout = [
+        GridItem(.flexible(minimum: 120)),
+        GridItem(.flexible(minimum: 120))
+    ]
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        NavigationView{
+            ScrollView(.vertical){
+                LazyVGrid(columns: layout, content: {
+                    ForEach(items, id: \.self) { item in
+                        
+                        Image(item)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .border(Color.secondary)
+                            .cornerRadius(4)
+                            .padding()
+                        
+                    }
+                })
+                
+                
+            }
+            
+        }
+        
+        
     }
-}
+    
+    
+    
+} // end View
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
