096f9f557911f533d3e7374b03d713bfbf18caac
ANDRES BECERRA <31633615+Andresbecerra72@users.noreply.github.com>
2021-08-19 16:29:37 -0500

    Initial Commit

---
+++ usergrid/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  usergrid
+//
+//  Created by Andres Becerra on 19/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ usergrid/usergridApp.swift
@@ -0,0 +1,17 @@
+//
+//  usergridApp.swift
+//  usergrid
+//
+//  Created by Andres Becerra on 19/08/21.
+//
+
+import SwiftUI
+
+@main
+struct usergridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
